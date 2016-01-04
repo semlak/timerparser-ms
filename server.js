f5f95548f9e32153fc7f3c71d634ed9d5eca0292
Joseph <semlak@gmail.com>
2016-01-04 11:04:38 -0600

    changed homepage to dynamic content with jade

--- server.js
+++ server.js
@@ -5,14 +5,30 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var port = process.env.PORT || 8080;
+var path = require('path');
+
 //app.engine('.html', require('jade'));
+app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-
+/*
 app.get('/', function(req, res) {
 	res.render('index.html');
     res.end();
+});
+*/
+
+
+app.get('/', function (req, res) {
+	console.log("rendering homepage");
+	//get page data
+	var pageData = require('./app/views/indexPageData.json');
+	var appURL = req.protocol + '://' + req.headers.host;
+	pageData = JSON.parse(JSON.stringify(pageData).replace(/APPURL/g, appURL));
+	// console.log(pageData);
+	res.render('index', pageData);
+	res.end();
 });
 
 app.get('/:timeInfo', function(req, res) {
